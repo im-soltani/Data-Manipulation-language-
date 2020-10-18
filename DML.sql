67e337c786ab8be51971ff99971591b69015b7a3
Imen Soltani <imensoltani6@gmail.com>
2020-10-19 00:17:40 +0200

    first commit

---
+++ DML.sql
@@ -0,0 +1,48 @@
+INSERT INTO Product VALUES
+ (
+   P01,
+   SAMSUNG GALAXY S20,
+   SMARTPHONE,
+   3299  
+ )
+ INSERT INTO Product VALUES
+ (
+   P02,
+   asus notbook,
+   PC,
+   4599  
+ )
+
+  INSERT INTO Customer VALUES
+ (
+   c01,
+   Ali,
+   71321009,
+   
+ )
+   INSERT INTO Customer VALUES
+ (
+   c02,
+   Asma,
+   77345823,
+   
+ )
+   INSERT INTO Orders VALUES
+ (
+   c01,
+   P02,
+   null,
+   2,
+   9198
+   
+ )
+ INSERT INTO Orders (Costumer_id,Product_id,Quantity,Total_amount) 
+VALUES (
+    
+  'C01',
+  'P02',
+   2, 
+   9468
+    
+);
+
